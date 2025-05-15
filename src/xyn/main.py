6b0b2aac5077d47ee0c634a3ce053381d409d352
Hilal Mufti <hmufti@cs.washington.edu>
2025-05-14 20:33:16 -0700

    enhancement: update todo list

--- src/xyn/main.py
+++ src/xyn/main.py
@@ -154,27 +154,30 @@ def main() -> None:
     print(show_list(dataset.tolist()))
 
 # TODO:
-# - [x] feat: remove delim argument
-# - [x] feat: add verbose mode
-# - [x] feat: rename verbose to all
-# - [ ] feat: add `xyn reg` subcommand
-# - [x] feat: add sum dataset
-# - [ ] fix: have sum dataset be exact sum (I think matmul is breaking it)
-# - [x] feat: add `xyn sum` subcommand
-# - [ ] feat: add sum dataset for integral valued inputs
-# - [ ] feat: add real-valued binary classification dataset
-# - [ ] feat: add header display option
-# - [ ] fix: fix scientific notation displaying
+# - [x] enhancement: remove delim argument
+# - [x] feat: implement verbose mode
+# - [x] enhancement: rename verbose to all
+# - [ ] enhancement: improve --help documentation for subcommands
+# - [ ] enhancement: improve README documentation for subcommands
+# - [ ] enhancement: remove n_outputs option for `xyn sum` subcommand
+# - [ ] feat: implement `xyn reg` subcommand
+# - [x] feat: implement sum dataset
+# - [ ] fix: sum dataset be exact sum (I think matmul is breaking it), but is inexact
+# - [x] feat: implement `xyn sum` subcommand
+# - [ ] feat: implement sum dataset for integral valued inputs
+# - [ ] feat: implement real-valued binary classification dataset
+# - [ ] feat: implement option to display header
+# - [ ] fix: array values should always print as decimals, but sometimes print in scientific notation
 # - [x] fix: fix emacs/magit git setup
 # - [x] feat: improve readme by showing how to use `xyn` with other unix commands
-# - [ ] feat: Add examples to `-h` help
-# - [ ] feat: improve typechecking of ndims
-# - [ ] feat: typechecking
-# - [ ] feat: tests
+# - [ ] enhancement: Add examples to `-h` help
+# - [ ] enhancement: improve typechecking of ndims
+# - [ ] enhancement: improve function typechecking and precondition checking
+# - [ ] enhancement: implement unit and expect tests
+# - [ ] enhancement: implement property-based testing
 # - [x] feat: multiple output variables
-# - [ ] fix: update readme for multiple output variables
-# - [ ] feat: binary classification
-# - [ ] feat: multi-dimensional classification
+# - [ ] enhancement: update readme for multiple output variables
+# - [ ] feat: implement multi-dimensional classification dataset
 # - [x] feat: train/test data (done by datasplit program)
 # - [ ] enhancement: refactor make_regression_dataset, make_sum_dataset
 # - [ ] enhancement: refactor parser_regression, parser_sum repeated code
